414ba71e7c5a7d059b4f5c18bafaf9fae5af5755
Jake Soenneker <jake@soenneker.com>
2025-05-20 09:39:42 -0500

    Initial commit

---
+++ src/Utils/Abstract/IOpenApiFixer.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Soenneker.GitHub.Runners.OpenApiClient.Utils.Abstract;
+
+public interface IOpenApiFixer
+{
+    ValueTask Fix(string sourceFilePath, string targetFilePath, CancellationToken cancellationToken = default);
+}
---
+++ test/Soenneker.GitHub.Runners.OpenApiClient.Tests/GitHubOpenApiClientRunnerTests.cs
@@ -0,0 +1,19 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.GitHub.Runners.OpenApiClient.Tests;
+
+[Collection("Collection")]
+public class GitHubOpenApiClientRunnerTests : FixturedUnitTest
+{
+
+    public GitHubOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
